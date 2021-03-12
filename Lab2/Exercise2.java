9e50f42764c257d21edd3e5ab72204650d04175c
feliksczart <czart78@gmail.com>
2021-03-12 20:22:57 +0100

    yikes

--- Lab2/Exercise2.java
+++ Lab2/Exercise2.java
@@ -1,74 +1,69 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.language.LanguageIdentifier;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.apache.tika.exception.TikaException;
-import org.apache.tika.langdetect.OptimaizeLangDetector;
-import org.xml.sax.SAXException;
-
-public class Exercise2
-{
+public class Exercise2 {
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private OptimaizeLangDetector langDetector;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Exercise2 exercise = new Exercise2();
         exercise.run();
     }
 
-    private void run()
-    {
-        try
-        {
-            if (!new File("./outputDocuments").exists())
-            {
+    private void run() {
+        try {
+            if (!new File("./outputDocuments").exists()) {
                 Files.createDirectory(Paths.get("./outputDocuments"));
             }
 
             initLangDetector();
 
-            File directory = new File("./documents");
+            File directory = new File("./documents/ok");
             File[] files = directory.listFiles();
-            for (File file : files)
-            {
+            assert files != null;
+            for (File file : files) {
                 processFile(file);
             }
-        } catch (IOException | SAXException | TikaException e)
-        {
+
+
+        } catch (IOException | TikaException | SAXException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void initLangDetector() throws IOException
-    {
+    private void initLangDetector() throws IOException {
         // TODO initialize language detector (langDetector)
     }
 
-    private void processFile(File file) throws IOException, SAXException, TikaException
-    {
+    private void processFile(File file) throws IOException, SAXException, TikaException {
         // TODO: extract content, metadata and language from given file
         // call saveResult method to save the data
 
-        saveResult(file.getName(), null, null, null, null, null, null); //TODO: fill with proper values
+        saveResult(file.getName(), getLanguage(file), null, null, null, null, null); //TODO: fill with proper values
     }
 
     private void saveResult(String fileName, String language, String creatorName, Date creationDate,
-                            Date lastModification, String mimeType, String content)
-    {
+                            Date lastModification, String mimeType, String content) {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
         int index = fileName.lastIndexOf(".");
         String outName = fileName.substring(0, index) + ".txt";
-        try
-        {
+        try {
             PrintWriter printWriter = new PrintWriter("./outputDocuments/" + outName);
             printWriter.write("Name: " + fileName + "\n");
             printWriter.write("Language: " + (language != null ? language : "") + "\n");
@@ -81,10 +76,25 @@ public class Exercise2
             printWriter.write("\n");
             printWriter.write(content + "\n");
             printWriter.close();
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
+    public String getLanguage(File file) throws TikaException, SAXException, IOException {
+
+        Parser parser = new AutoDetectParser();
+        BodyContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        if (file.isFile()) {
+            FileInputStream content = new FileInputStream(file);
+            //Parsing the given document
+            parser.parse(content, handler, metadata, new ParseContext());
+
+            LanguageIdentifier object = new LanguageIdentifier(handler.toString());
+            System.out.println(file + " Language name: " + object.getLanguage());
+            return object.getLanguage();
+        }
+        else return null;
+    }
 }
